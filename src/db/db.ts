4cb3632f637cf14ed7c62ac7e752c5a9a58aaf95
Dalerbek Artikmuratov <141311341+artikmuratovdev@users.noreply.github.com>
2026-03-03 14:27:12 +0500

    Update image URLs and paths in db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,30 +1,30 @@
 export const courses = [
   {
-    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo02zXPyZ51xPWtVAgLib0hZoQDvKECnGHuYdBj&w=1920&q=75&dpl=dpl_9BY4WfzXujZSdQGVHZ8qhj3KUjH9w&s=',
+    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo00Gmjkh79cpx21MsQhvWPLIgeAHGwlaumbU8j&w=3840&q=75',
     title : "Foundation"
   },
   {
-    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo06tUZZyJ3zGAQDL8Fg0WfZjoJNTunX9qViPby&w=1920&q=75&dpl=dpl_9BY4WfzXujZSdQGVHZ8qhj3KUjH9w&s=',
+    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo01pCyzWVJWIAxq9EjKgNoL7zftvlDbOmru0H2&w=1920&q=75',
     title : "JavaScript"
   },
   {
-    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0pu6T6XIV0jo2XA16Ydy4rSfwLIiRxv98gOuk&w=1920&q=75&dpl=dpl_9BY4WfzXujZSdQGVHZ8qhj3KUjH9w&s=',
+    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0E5lF6amAU8kioLXqN3YRt4exAMZ2yFscwGIl&w=1920&q=75',
     title : "TypeScript"
   },
   {
-    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0hd5vTF0SS4EXWoPYTwZe8J6lmbK01atBMDu9&w=1920&q=75&dpl=dpl_9BY4WfzXujZSdQGVHZ8qhj3KUjH9w&s=',
+    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0iUp6fYhocXUlvFEwuZWOT81zHSksDrGxN4f9&w=1920&q=75',
     title : "ReactJS"
   },
   {
-    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0CNteTKXWxwSDyGP6XiNAu4CEJnc7O1V9ZLth&w=1920&q=75&dpl=dpl_9BY4WfzXujZSdQGVHZ8qhj3KUjH9w&s=',
+    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo041nrlbd9utCZemPzyoJSOTAbr18IvF0MYQ6H&w=1080&q=75',
     title : "Next JS"
   },
   {
-    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0Kn8MKEqFyPNMizqY8HXwlBmVKp5odv9gEc0I&w=1920&q=75&dpl=dpl_9BY4WfzXujZSdQGVHZ8qhj3KUjH9w&s=',
+    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0pJtmiWIV0jo2XA16Ydy4rSfwLIiRxv98gOuk&w=2048&q=75',
     title : "VueJS"
   },
   {
-    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0MHB5U91EYDf8RF6ISJTyzGbr41Qa73CBOsvN&w=1920&q=75&dpl=dpl_9BY4WfzXujZSdQGVHZ8qhj3KUjH9w&s=',
+    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0Dt6eWDpQTl9a4CLgOmnEFkfdiNbwx068Xc3o&w=1200&q=75',
     title : "NodeJS"
   },
   {
@@ -32,37 +32,41 @@ export const courses = [
     title : "Murakkab Backend"
   },
   {
-    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0i4CifD7hocXUlvFEwuZWOT81zHSksDrGxN4f&w=1920&q=75&dpl=dpl_9BY4WfzXujZSdQGVHZ8qhj3KUjH9w&s=',
+    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0WO04xMuv70rfVnCNDiebGQXS2lh4oIUJ8tdw&w=1080&q=75',
     title : "NestJS"
   },
   {
-    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0Dlnm5upQTl9a4CLgOmnEFkfdiNbwx068Xc3o&w=1920&q=75&dpl=dpl_9BY4WfzXujZSdQGVHZ8qhj3KUjH9w&s=',
+    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0iUp6fYhocXUlvFEwuZWOT81zHSksDrGxN4f9&w=1920&q=75',
     title : "React Native"
   },
   {
-    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo07J4r1kwIoTnBLuJKO1XdgS3bxpW5CcPV0iqH&w=1920&q=75&dpl=dpl_9BY4WfzXujZSdQGVHZ8qhj3KUjH9w&s=',
+    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fqk9g5hsrut.ufs.sh%2Ff%2Fxu965PmyvPo0FM6vDAPKdjJrMesifW4Gtho7ID1zXTlQ025w&w=1920&q=75',
     title : "Telegram Bot"
+  },
+  {
+    img : 'https://www.sammi.ac/_next/image?url=https%3A%2F%2Fwww.sammi.ac%2F_next%2Fimage%3Furl%3Dhttps%253A%252F%252Fqk9g5hsrut.ufs.sh%252Ff%252Fxu965PmyvPo029yCLv51xPWtVAgLib0hZoQDvKECnGHuYdBj%26w%3D1920%26q%3D75%26dpl%3Ddpl_AyWjnwefx2RFAeEprCzJhU8VkTuz&w=2048&q=75',
+    title : "Angular"
   },
 ]
 
 export const projects = [
   {
-    img:'../../../public/download.png',
+    img:'../assets/download.png',
     title: "Expense Tracker",
     link : "https://expense-tracker-savitar-uzuhiko.vercel.app/"
   },
   {
-    img:'../../../public/download (1).png',
+    img:'../assets/download (1).png',
     title: "Artemis Project",
     link : "https://artemis-v3.vercel.app/"
   },
   {
-    img:'../../../public/download (2).png',
+    img:'../assets/download (2).png',
     title: "Admin Dashboard",
     link : "https://dashboard-silk-beta.vercel.app/"
   },
   {
-    img:'../../../public/download (3).png',
+    img:'../assets/download (3).png',
     title: "Memory Card",
     link : "https://memory-card-game-kappa-taupe.vercel.app/"
   },
